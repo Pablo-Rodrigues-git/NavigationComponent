cd2eeac47f875f13ae0c2f6090c00648e055458d
Pablo Rodrigues <pablorodrigues@mobills.com.br>
2022-04-12 13:38:52 -0300

    Creating first navigation between screens

--- app/src/main/java/com/training/ui/navigationcomponent/FirstFragment.kt
+++ app/src/main/java/com/training/ui/navigationcomponent/FirstFragment.kt
@@ -1,8 +1,21 @@
 package com.training.ui.navigationcomponent
 
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
 class FirstFragment : Fragment(R.layout.fragment_first) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        view.findViewById<TextView>(R.id.tv_id).setOnClickListener {
+
+            findNavController().navigate(R.id.fromFirstFragmentToSecondFragment)
+
+        }
+
+    }
 
 }
--- app/src/main/java/com/training/ui/navigationcomponent/SecondFragment.kt
+++ app/src/main/java/com/training/ui/navigationcomponent/SecondFragment.kt
@@ -5,9 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
 
 class SecondFragment : Fragment(R.layout.fragment_second) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        view.findViewById<TextView>(R.id.tv_id).setOnClickListener {
+
+            findNavController().navigate(R.id.fromSecondFragmentToFirstFragment)
+
+        }
+
+    }
 
 
 }
