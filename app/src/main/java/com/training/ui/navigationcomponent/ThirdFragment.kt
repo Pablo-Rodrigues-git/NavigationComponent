59dd9646dc9d0c746bb5b7ef30732ea7ae8b8e31
Pablo Rodrigues <pablorodrigues@mobills.com.br>
2022-04-14 00:54:48 -0300

    creating third card, modifying fragment_second.xml fragment_third.xml layouts, creating another actions

--- app/src/main/java/com/training/ui/navigationcomponent/ThirdFragment.kt
+++ app/src/main/java/com/training/ui/navigationcomponent/ThirdFragment.kt
@@ -1,21 +1,18 @@
 package com.training.ui.navigationcomponent
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
 import androidx.cardview.widget.CardView
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 
-class SecondFragment : Fragment(R.layout.fragment_second) {
+class ThirdFragment : Fragment(R.layout.fragment_third) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         view.findViewById<CardView>(R.id.cardId).setOnClickListener {
 
-            findNavController().navigate(R.id.fromSecondToThird)
+            findNavController().navigate(R.id.fromThirtToFirst)
 
         }
 
